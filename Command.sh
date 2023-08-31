1e347ba93e79914d44a6776dddcb18148b29c863
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-08-31 19:51:47 +0000

    find history of commands input

--- Command.sh
+++ Command.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
-man ls
+history
--- Commit.sh
+++ Commit.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "find ls manual" # add commit message
+git commit -m "find history of commands input" # add commit message
 git push
